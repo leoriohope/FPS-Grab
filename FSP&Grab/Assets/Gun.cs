ae85167103656d2ad829f1fb60364b75c84bb76c
Liangding Li <leoriohope@gmail.com>
2019-11-11 22:03:57 -0500

    Move from mac to PC

---
+++ FSP&Grab/Assets/Gun.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 15f;
+    public GameObject hitEffect;
+    float hitForce = 300f;
+
+    private float nextTimeToFire = 0f;
+
+    public Camera fpscam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextTimeToFire) {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+
+    }
+
+    void Shoot() {
+        RaycastHit hit;
+        if (Physics.Raycast(fpscam.transform.position, fpscam.transform.forward, out hit, range)) {
+            Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null) {
+                target.TakeDamage(damage);
+            }
+        }
+
+        if (hit.rigidbody != null) {
+            hit.rigidbody.AddForce(-hit.normal * hitForce);
+        } 
+
+        
+        GameObject hitFire = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(hitFire, 2f);
+    }
+
+}
